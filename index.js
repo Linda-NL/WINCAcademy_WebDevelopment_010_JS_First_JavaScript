1077ef737e599006c197a9302289426e49bf021f
Linda <86129384+Linda-NL@users.noreply.github.com>
2021-07-20 14:36:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+//OPDRACHT 4
+console.log('Hello Winc Academy')
+
+//OPDRACHT 5
+let firstName = 'Linda';
+console.log(firstName)
+
+//OPDRACHT 6
+// je kunt geen variable maken met de naam if, omdat if een reserved word is
+
+//OPDRACHT 7
+// 2 numbers optellen
+let numberAddition = 4 + 4;
+console.log(numberAddition)
+
+// 2 strings optellen
+let stringAddition = '4' + '4';
+console.log(stringAddition)
+
+//OPDRACHT 8
+//addition test +
+numberAddition = 10 + 3;
+console.log(numberAddition)
+
+//substraction test -
+let numberSubstraction = 10 - 3;
+console.log(numberSubstraction)
+
+//multiplication test *
+let numberMultiplication = 10 * 3;
+console.log(numberMultiplication)
+
+//division test /
+let numberDivision = 10 / 3;
+console.log(numberDivision)
+
+//modulus test % (=rest, wat overblijft na het delen, bv. 10 delen door 3 = 3 rest 1. 1 is hierbij de modulus)
+let numberModulus = 10 % 3;
+console.log(numberModulus)
+
+//OPDRACHT 9
+let age = 37;
+console.log(typeof age)
+
+//OPDRACHT 10
+age = 'zevenendertig';
+console.log(typeof age)
